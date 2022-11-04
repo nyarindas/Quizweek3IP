70afc0e9c112fa0a6a3bfdac113bcb67df1e19d5
Sharon Nyarinda <nyarindas@gmail.com>
2022-11-04 17:46:57 +0300

    function

--- script.js
+++ script.js
@@ -6,11 +6,32 @@ function submitted(e) {
 
   function result(){
     var score=0;
-        if (document.getElementById('correct1').checked)
+    if (document.getElementById('correct1').checked)
     {
         score++
 
     }
+    if (document.getElementById('correct2').checked)
+    {
+        score++
+
+    }
+    if (document.getElementById('correct3').checked)
+    {
+        score++
+
+    }
+    if (document.getElementById('correct4').checked)
+    {
+        score++
+
+    }
+    if (document.getElementById('correct5').checked)
+    {
+        score++
+
+    }
+   
 
 
 
