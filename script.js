204b2e3a365b76535aa75e8cddda30dea3ec6ffa
Sharon Nyarinda <nyarindas@gmail.com>
2022-11-04 17:45:31 +0300

    jsadd

--- script.js
+++ script.js
@@ -3,3 +3,15 @@ function submitted(e) {
     let namee = document.forms["id"]["name"].value;
     sessionStorage.setItem("name", namee);
   }
+
+  function result(){
+    var score=0;
+        if (document.getElementById('correct1').checked)
+    {
+        score++
+
+    }
+
+
+
+  }
